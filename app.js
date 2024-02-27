d9dadd3294af690082a311a8d664a196c85117e0
tttiia <152015037+tttiia@users.noreply.github.com>
2024-02-27 15:31:40 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+ 
+ 
+var app = express();
+ 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+ 
+module.exports = app;
+
+const bookRouter = require('./routes/book');
+app.use('/book', bookRouter);
+
+const borrowerRouter = require('./routes/borrower');
+app.use('/borrower', borrowerRouter);
+
+
+
+
+
+
+
